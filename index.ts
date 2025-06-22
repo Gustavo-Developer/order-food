10f094519b8ab042e89677fe8aa14c8b899b871b
gustavo-developer <srto.mendes@gmail.com>
2025-06-21 23:21:05 -0300

    init server

--- index.ts
+++ index.ts
@@ -3,8 +3,8 @@ import express, { Request, Response } from 'express';
 const app = express();
 
 
-app.use('/', (req: Request, res: Response) => {
-  return res.json('Food order API');
+app.use('/', (_req: Request, res: Response) => {
+  res.json('Food order API');
 });
 
 app.listen('8000', () => {
